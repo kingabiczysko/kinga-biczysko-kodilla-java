3e32c75fbfeb9ba6782b8d69b90ec3017ba9c5a2
kingabiczysko <kinga.biczysko@gmail.com>
2017-11-21 10:19:29 +0100

    Zadanie 6.4. Update 4

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -11,20 +11,32 @@ public class ShapeCollector {
 
     //1
     public void addFigure(Shape shape){
-        //do nothing temp
+        list.add(shape);
     }
 
 
     //2
     public boolean removeFigure (Shape shape){
-       return true; //temporary
+        boolean result = false;
+        if(list.contains(shape))
+        {
+            list.remove(shape);
+            result = true;
+        }
+        return result;
     }
 
     //3
     public Shape getFigure (int n){
-        return null; //temp
+        Shape shape = null;
+        if(n >= 0 && n <list.size()){
+            shape = list.get(n);
+        }
+        return shape;
     }
 
+
+    //4
     public int showFigures(){
         System.out.println(list.toArray());
         return list.size();
