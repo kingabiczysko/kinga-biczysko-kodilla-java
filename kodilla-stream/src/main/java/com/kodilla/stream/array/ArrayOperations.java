66b0d26771dc38835f3bb801efb3fbd073ea3e35
kingabiczysko <kinga.biczysko@gmail.com>
2017-11-28 13:54:49 +0100

    Zadanie 7.5.

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,27 @@
+package com.kodilla.stream.array;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+
+    static double getAverage(int[] numbers) {
+
+
+
+
+
+       double average = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average()
+               .getAsDouble();
+
+
+
+
+
+       return average;
+
+    }
+}
+
+
