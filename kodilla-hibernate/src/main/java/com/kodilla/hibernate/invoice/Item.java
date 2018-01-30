d0e078b14a294880b4eea43695df558301b46ca0
kingabiczysko <kinga.biczysko@gmail.com>
2018-01-30 13:19:21 +0100

    Zadanie 13.3.

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Item.java
@@ -0,0 +1,82 @@
+package com.kodilla.hibernate.invoice;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ITEM")
+public class Item {
+    private int id;
+    private Product product;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal value;
+
+    public Item() {
+    }
+
+    public Item(Product product, BigDecimal price, int quantity, BigDecimal value) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.value = value;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ITEM_ID")
+    public int getId() {
+        return id;
+    }
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "PRODUCT",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(name = "PRODUCT_ID")
+    public Product getProduct() {
+        return product;
+    }
+
+    @NotNull
+    @Column(name = "ITEM_PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @NotNull
+    @Column(name = "ITEM_QUANTITY")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @NotNull
+    @Column(name = "ITEM_VALUE")
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setProduct(Product product) {
+        this.product = product;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    private void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    private void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
