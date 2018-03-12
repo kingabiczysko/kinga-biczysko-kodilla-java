51369c5dc927ff79518dc30b1fae32e5e77da382
kingabiczysko <kinga.biczysko@gmail.com>
2018-03-12 13:57:48 +0100

    Binary2

--- kodilla-testing/src/main/java/com/kodilla/testing/binary/Binary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/binary/Binary.java
@@ -1,21 +1,16 @@
 package com.kodilla.testing.binary;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
 public class Binary {
 
 
     public double changeIntoInt(int binary){
         String binaryString = String.valueOf(binary);
         int l = binaryString.length();
-        int [] tabelaBinary = new int[l];
-        int [] tabelaPotega1 = new int[l];
-        int [] tabelaPotega2 = new int[l];
-        int [] tabelaFinal = new int[l];
+        int result = 0;
+        int potega = 0;
 
 
-        tabelaBinary = binaryString.chars().toArray();
+        int [] tabelaBinary = binaryString.chars().toArray();
 
         for(int n=0; n<l; n++) {
             if (tabelaBinary[n] == 49) {
@@ -23,18 +18,13 @@ public class Binary {
             } else tabelaBinary[n] = 0;
         }
 
-        for (int n=0;n<l;n++){
-            tabelaPotega1[n] = (int)Math.pow(2,n);
+        for(int i=tabelaBinary.length-1; i>=0; i--){
+            if(tabelaBinary[i] == 1){
+                result += (int) Math.pow(2,potega);
+            }
+            potega++;
         }
 
-        tabelaPotega2 = IntStream.rangeClosed(1,l).map(i->tabelaPotega1[l-i]).toArray();
-
-        for(int n=0; n<l; n++) {
-            tabelaFinal[n] = tabelaBinary[n] * tabelaPotega2[n];
-        }
-
-        return (Arrays.stream(tabelaFinal)
-                .sum());
-
+        return result;
     }
 }
